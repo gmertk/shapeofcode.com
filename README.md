968acc4df1068365f1668f265b923003c36da66a
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2020-12-12 15:40:44 +0000

    Update blog name

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-<p align="center">
-  <a href="https://minimal-blog.lekoarts.de">
-    <img alt="Shape of Code" src="" />
-  </a>
-</p>
 <h1 align="center">
-  A Blog by Mert on Programming and Web Development
+  My Blog on Mobile and Web Development
 </h1>
 
-<p align="center">
-  <a href="https://twitter.com/intent/follow?screen_name=gunaymertk">
-    <img src="https://img.shields.io/twitter/follow/gunaymertk.svg?label=Follow%20@gunaymertk" alt="Follow @gunaymertk" />
-  </a>
-</p>
-
-Using this Gatsby Theme [`@lekoarts/gatsby-theme-minimal-blog`](https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog).
+Using Gatsby Theme [`@lekoarts/gatsby-theme-minimal-blog`](https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog).
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,10 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `Shape of Code`,
+    siteTitle: `Shape of My Code`,
     siteTitleAlt: `A blog by Günay Mert Karadoğan`,
     siteHeadline: `But that's not the shape of my code...`,
-    siteUrl: "https://shapeofcode.com",
+    siteUrl: "https://shapeofmycode.com",
     siteDescription: `A blog by Mert on programming, design, web development, JavaScript, TypeScript, and so on`,
     siteLanguage: `en`,
     siteImage: "",
@@ -48,8 +48,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Shape of Code - A blog by Günay Mert Karadoğan`,
-        short_name: `shape-of-code`,
+        name: `Shape of My Code - A blog by Günay Mert Karadoğan`,
+        short_name: `shape-of-my-code`,
         description: `A blog by Mert on programming, design, web development, JavaScript, TypeScript, and so on`,
         start_url: `/`,
         background_color: `#fff`,
